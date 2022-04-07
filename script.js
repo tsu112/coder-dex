f06d1a917f4703fede6a82be6659de8569d6d6c2
Sue Yang <sueyang112@hotmail.com>
2022-04-07 16:50:09 -0400

    Finished

---
+++ script.js
@@ -0,0 +1,27 @@
+var cardsDiv = document.querySelector("#cards");
+
+var currentUsername = "";
+
+function getUsername(element) {
+  console.log(element.value);
+  currentUsername = element.value;
+}
+
+function makeCoderCard(data) {
+  var res = `<div class = "card">
+                <img src="${data.avatar_url}" alt="${data.login}">
+                <div>
+                <h3>${data.login} - ${data.name}</h3> 
+                <p>Location: ${data.location}</p>
+                <p>Repositories: ${data.public_repos}</p>
+                </div>
+            </div>`;
+  return res;
+}
+
+async function search() {
+  var response = await fetch("https://api.github.com/users/" + currentUsername);
+  var coderData = await response.json();
+  console.log(coderData);
+  cardsDiv.innerHTML = makeCoderCard(coderData) + cardsDiv.innerHTML;
+}
